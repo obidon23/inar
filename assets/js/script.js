0b6beaef4a73b2c510dcdf4cd21e548fccc7d606
Don Guckenberger <dguck23@gmail.com>
2017-08-03 16:50:31 -0600

    update form, styled divs, removed scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,9 +104,10 @@ function validateForm() {
   teamName = $("#teamName").val().trim();
   captainName = $("#captainName").val().trim();
   captainEmail = $("#captainEmail").val().trim();
+  captainPhone = $("#captainPhone").val().trim();
   deputyName = $("#deputyName").val().trim();
   deputyEmail = $("#deputyEmail").val().trim();
-  if ( captainName && captainEmail && deputyName && deputyEmail && teamName) {
+  if (captainName && captainEmail && captainPhone && deputyName && deputyEmail &&teamName) {
       pushForm();
  
 }
@@ -120,26 +121,23 @@ setup();
 
 
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
-    console.log(this.hash);
-    debugger;
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+  // $("li a").on('click', function(event) {
 
-      // Store hash
-      var hash = this.hash;
-      console.log(hash);
-      debugger;
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+  //   // Make sure this.hash has a value before overriding default behavior
+  //   if (this.hash !== "") {
+  //     // Prevent default anchor click behavior
+  //     event.preventDefault();
+
+  //     // Store hash
+  //     var hash = this.hash;
+  //     // Using jQuery's animate() method to add smooth page scroll
+  //     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+  //     $('html, body').animate({
+  //       scrollTop: $(hash).offset().top
+  //     }, 800, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+  //       // Add hash (#) to URL when done scrolling (default click behavior)
+  //       window.location.hash = hash;
+  //     });
+  //   } // End if
+  // });
